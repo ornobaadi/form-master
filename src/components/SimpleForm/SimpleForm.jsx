37766d7f8d0b5fd655eecba03cff4e3d439575d1
ornobaadi <aadi4789@gmail.com>
2024-11-02 01:54:53 +0600

    first commit

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,27 @@
+
+const SimpleForm = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.phone.value)
+        console.log('Form Submitted');
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="text" name="phone" id="" />
+                <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
