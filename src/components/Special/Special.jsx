91c5cdd1716f5298c6a65255a7708b4194e1be32
ornobaadi <aadi4789@gmail.com>
2024-11-02 05:24:54 +0600

    Pop Drilling & useContext

--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { AssetContext } from "../Grandpa/Grandpa";
 
-const Special = () => {
+const Special = ({asset}) => {
+    const gift = useContext(AssetContext)
+
     return (
         <div>
             <h2>Special Person</h2>
+            <p>Has : {asset}</p>
+            <p>Has : {gift}</p>
         </div>
     );
 };
